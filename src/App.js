495017068b16ec7129148cbb1a5de0a56cea871a
EnPere <pereruba@gmail.com>
2023-02-10 16:27:09 +0100

    reestructura de todo

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import faviconLogo from "./favicon.ico";
 import ListaDeTareas from "./components/ListaDeTareas";
 
 function App() {
-  return (
-    <div className="aplicacion-tareas">
-      <div className="my-logo-container">
-        <img src={faviconLogo} className="my-logo" alt="mylogo" />
-      </div>
-      <div className="tareas-lista-principal">
-        <h1>To Do List</h1>
-        <ListaDeTareas />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
